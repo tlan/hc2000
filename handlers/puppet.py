d5d87badad99a94663fcbfd71737de8f3eb66719
João Abecasis <joao@abecasis.name>
2013-06-24 13:50:57 +0200

    Split puppet handler for CloudInit into its own file

---
+++ handlers/puppet.py
@@ -0,0 +1,20 @@
+#part-handler
+
+def list_types():
+    return [ 'text/puppet' ]
+
+def handle_mime_begin(data, filename, payload):
+    data.part_handlers['text/cloud-config'](data, 'text/cloud-config', 'hc2000-puppet-install',
+            '\n'.join([ '#cloud-config', 'packages: [ puppet ]', '' ]))
+
+def handle_mime_puppet(data, filename, payload):
+    data.part_handlers['text/x-shellscript'](data, 'text/x-shellscript', filename, '#!/usr/bin/puppet apply\n' + payload)
+
+_handlers = {
+    '__begin__': handle_mime_begin,
+    'text/puppet': handle_mime_puppet,
+}
+
+def handle_part(data, ctype, filename, payload):
+    if ctype in _handlers:
+        _handlers[ctype](data, filename, payload)
