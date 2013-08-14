8d3de9cc60a8988d9b1d2b4de37f2a45a1d172f7
João Abecasis <joao@abecasis.name>
2013-08-14 22:18:52 +0200

    Add 'cloud-config' plugin
    As it turns out, there can only be one cloud-config file per instance
    otherwise CloudInit will misbehave or, more likely, silently drop
    commands. This means the earlier puppet support is broken when used
    together with explicit #cloud-config entries in user-data.
    
    With this, we make cloud-config a first-class citizen in instance
    configuration, which also enables some validation and compression of the
    resulting yaml. For the time being, the only validation provided is the
    requirement of valid yaml.

---
+++ hc2002/plugin/cloud_config.py
@@ -0,0 +1,18 @@
+import hc2002.plugin as plugin
+import yaml
+
+plugin.register_for_resource(__name__, 'hc2002.resource.instance')
+
+def apply(instance):
+    if 'cloud-config' not in instance:
+        return
+
+    if 'user-data' not in instance:
+        instance['user-data'] = []
+    if isinstance(instance['user-data'], basestring):
+        instance['user-data'] = [ instance['user-data'] ]
+
+    instance['user-data'].append('#cloud-config\n' \
+            + yaml.safe_dump(instance['cloud-config']))
+
+    del instance['cloud-config']
