4bb9d57746d66a0c879d6bd21678b1325b38b256
João Abecasis <joao@abecasis.name>
2013-08-14 15:16:30 +0200

    Move CloudInit handlers inside hc2002 library

--- hc2002/config.py
+++ hc2002/config.py
@@ -3,3 +3,5 @@ import os
 region = os.environ.get('EC2_REGION')
 aws_access_key = os.environ.get('AWS_ACCESS_KEY')
 aws_secret_key = os.environ.get('AWS_SECRET_KEY')
+
+handler_path = os.path.join(os.path.dirname(__file__), 'handler')
