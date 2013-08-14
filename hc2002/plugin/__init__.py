0f3214ea41d9ba50697cd982180d77bdb43c65e3
João Abecasis <joao@abecasis.name>
2013-08-14 20:51:06 +0200

    Move manifest handling to an independent "plugin"
    This introduces a customization layer on top of the basic resources,
    namely plugins that pre-process resource data. One benefit is the
    ability to move existing customizations inside the library, another is
    the possibility of expressing plugin dependencies as python imports.
    
    It doesn't make everything pretty right away :-p

---
+++ hc2002/plugin/__init__.py
@@ -0,0 +1,25 @@
+import importlib
+
+def _resolve(resource):
+    if isinstance(resource, basestring):
+        return importlib.import_module(resource)
+    return resource
+
+def register_for_resource(plugin, resource):
+    resource = _resolve(resource)
+
+    if not hasattr(resource, 'plugins'):
+        resource.plugins = []
+    resource.plugins.insert(0, plugin)
+
+def apply_for_resource(resource, data):
+    if not hasattr(resource, 'plugins'):
+        return
+
+    for plugin in resource.plugins:
+        plugin = _resolve(plugin)
+
+        if not hasattr(plugin, 'apply'):
+            continue
+
+        plugin.apply(data)
---
+++ hc2002/plugin/manifest.py
@@ -0,0 +1,65 @@
+import hc2002.config as config
+import hc2002.manifest
+import hc2002.plugin as plugin
+from hc2002.validation import validate
+import os.path
+import yaml
+
+plugin.register_for_resource(__name__, 'hc2002.resource.instance')
+
+def _add_handler(instance):
+    filename = os.path.join(config.handler_path, 'manifest.py')
+    with open(filename, 'rb') as f:
+        handler = f.read()
+    if not handler.startswith('#part-handler'):
+        handler = '#part-handler\n' + handler
+    instance['user-data'].append(handler)
+
+def _process_manifest_tag(instance):
+    if 'manifest' not in instance:
+        return
+
+    if 'user-data' not in instance:
+        instance['user-data'] = []
+    if isinstance(instance['user-data'], basestring):
+        instance['user-data'] = [ instance['user-data'] ]
+
+    _add_handler(instance)
+
+    manifest = instance['manifest']
+    if not isinstance(manifest, basestring):
+        manifest = yaml.dump(manifest)
+    if not manifest.startswith(('file:', '#manifest\n')):
+        manifest = '#manifest\n' + manifest
+
+    instance['user-data'].append(manifest)
+
+    del instance['manifest']
+
+def _validate_manifests(instance):
+    if 'user-data' not in instance:
+        return
+
+    if isinstance(instance['user-data'], basestring):
+        instance['user-data'] = [ instance['user-data'] ]
+
+    for i, entry in enumerate(instance['user-data']):
+        if not isinstance(entry, basestring):
+            continue
+        scope = 'user-data[%i]' % i
+
+        #FIXME: Should either move file inlining earlier or validation
+        #       later. Should NOT read the file twice. Live as plugin of a
+        #       user-data plugin? :-)
+        if entry.startswith('file:'):
+            scope += ':%s:' % entry[5:]
+            entry = open(entry[5:], 'rb').read()
+
+        if entry.startswith('#manifest\n'):
+            print "Validating manifest:\n", entry
+            validate(hc2002.manifest, yaml.safe_load(entry), scope) \
+                    or sys.exit(1)
+
+def apply(instance):
+    _process_manifest_tag(instance)
+    _validate_manifests(instance)
