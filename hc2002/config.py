7e977c9d8e412309e798a9873dac3ac9a5bf5c44
João Abecasis <joao@abecasis.name>
2013-12-02 02:14:28 +0100

    Accept AWS_DEFAULT_REGION environment variable
    The variable is recognized by the AWS CLI tool, as well.

--- hc2002/config.py
+++ hc2002/config.py
@@ -1,6 +1,7 @@
 import os
 
-region = os.environ.get('EC2_REGION')
+region = os.environ.get('AWS_DEFAULT_REGION') \
+        or os.environ.get('EC2_REGION')
 aws_access_key = os.environ.get('AWS_ACCESS_KEY')
 aws_secret_key = os.environ.get('AWS_SECRET_KEY')
 
