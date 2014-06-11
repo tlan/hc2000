509033cf3c22822de04458828fca0a549201af0c
Thomas Langaas <tlan@comoyo.com>
2014-06-11 21:48:05 +0200

    Remove boto.s3 as dep. from manifest handler
    This will make sure the handler works even in environments where
    python-boto package hasn't been installed yet.  The replacement
    is really simple and far from complete.

--- hc2002/handler/manifest.py
+++ hc2002/handler/manifest.py
@@ -1,13 +1,112 @@
-import boto.s3.connection
-import boto.s3.bucket
-import boto.s3.key
+import base64
+import email.utils
 import grp
+import hashlib
+import hmac
+import json
+import logging
 import os
 import os.path
 import pwd
 import re
+import urllib
 import urllib2
 import yaml
+
+# Setup the logger
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+
+def _get_aws_metadata(path, timeout=10):
+    md_url = "http://169.254.169.254/latest/meta-data/"
+    response = urllib2.urlopen(md_url + path, timeout)
+    return response.read()
+
+def _get_s3_endpoint():
+    """Returns correct s3 url based upon where the instance is located"""
+    try:
+        availability_zone = _get_aws_metadata("placement/availability-zone")
+    except:
+        # Default to eu-west-1
+        return 'http://s3-eu-west-1.amazonaws.com'
+    if availability_zone.startswith("us-east-1"):
+        return 'http://s3.amazonaws.com'
+    region = availability_zone[0:9]
+    return 'http://s3-%s.amazonaws.com' % region
+
+
+class S3Connection:
+    """Connection to S3"""
+    def __init__(self, access_key=None, secret_key=None, security_token=None):
+        self.endpoint = _get_s3_endpoint()
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.security_token = security_token
+
+    def request(self, bucket, path, query=None):
+        headers = { 'Host': bucket }
+        self._authenticate_request(bucket, path, headers)
+        url = self.endpoint + path + (query or '')
+        LOG.info("S3 Request: %s (headers: %s)", url, headers)
+        request = urllib2.Request(url, headers=headers)
+        response = urllib2.urlopen(request)
+        if response.getcode() == 200:
+            return response
+
+        raise RuntimeError('%s: %s' % (response.getcode(), response.read()))
+
+    def _authenticate_request(self, bucket, path, headers):
+        # See http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
+        # Shortcuts taken liberally, this is not a full implementation.
+
+        if not self.access_key:
+            return
+
+        date = email.utils.formatdate()
+        headers['Date'] = date
+
+        message = 'GET\n\n\n' + date + '\n'
+        if self.security_token:
+            headers['x-amz-security-token'] = self.security_token
+            message += 'x-amz-security-token:' + self.security_token + '\n'
+        message += '/' + bucket + path
+
+        h = hmac.new(self.secret_key, message, hashlib.sha1)
+        signature = base64.b64encode(h.digest())
+
+        headers['Authorization'] = 'AWS %s:%s' % (self.access_key, signature)
+
+class S3Object:
+    """Simple representation of an object found in S3"""
+    def __init__(self, md5hash, filestream):
+        self.md5hash = md5hash
+        self.filestream = filestream
+
+    def get_contents_to_file(self, output_fp):
+        """Write the contents of the s3 object to local file"""
+        LOG.info("Writing %s to local file %s.", self.filestream.url, output_fp.name)
+        for data in iter(lambda: self.filestream.read(32768), ""):
+            output_fp.write(data)
+
+class S3Bucket:
+    """A read-only, view over a flat directory in AWS S3."""
+
+    def __init__(self, s3connection, bucket, prefix=''):
+        self.s3 = s3connection
+        self.bucket = bucket
+        self.prefix = prefix + '/'
+
+    def get_object(self, name):
+        """Read object from S3.
+
+        Returns a S3Object
+        """
+        path = urllib.quote_plus(self.prefix + name, '/')
+        response = self.s3.request(self.bucket, path)
+
+        return S3Object(response.headers['ETag'][1:-1], response)
+
 
 def _mode_x_from_r(mode):
     return mode | (mode & 0444) >> 2
@@ -25,32 +124,30 @@ def _mk_parents(data, parts):
         except OSError:
             pass
 
-def _get_url(url):
-    response = urllib2.urlopen(url)
-    return response.read()
-
 def _get_key(data, source):
     bucket, _, key = source[len('s3://'):].partition('/')
     if data.s3 is None:
-        md_url = 'http://169.254.169.254/latest/meta-data/iam/security-credentials'
+        md_url = 'iam/security-credentials'
 
-        role = _get_url(md_url)
-        creds = yaml.safe_load(_get_url(md_url + '/' + role))
+        role = _get_aws_metadata(md_url)
+        creds = yaml.safe_load(_get_aws_metadata(md_url + '/' + role))
 
-        data.s3 = boto.s3.connection.S3Connection(
-                aws_access_key_id=creds['AccessKeyId'],
-                aws_secret_access_key=creds['SecretAccessKey'],
-                security_token=creds['Token'])
+        data.s3 = S3Connection(
+                creds["AccessKeyId"],
+                creds["SecretAccessKey"],
+                creds["Token"])
 
     if bucket not in data.buckets:
-        data.buckets[bucket] = boto.s3.bucket.Bucket(data.s3, bucket)
-    return boto.s3.key.Key(data.buckets[bucket], key)
+        data.buckets[bucket] = S3Bucket(data.s3, bucket)
+    return data.buckets[bucket].get_object(key)
 
 def _fetch_file(data, source, file):
     if source.startswith('s3://'):
+        LOG.info("Fetching from S3: %s", source)
         key = _get_key(data, source)
         key.get_contents_to_file(file)
     else:
+        LOG.error("URI scheme not implemented while fetching %s", source)
         raise NotImplementedError
 
 def _mk_file(data, path, source, content, target):
